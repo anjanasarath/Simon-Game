c105bbe54d8dc01b304a938fdf7006b6e88b2be4
Anjana Sarath <anjanasarath5@gmail.com>
2021-04-09 22:08:50 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,66 @@
+
+let buttonColors = ["red","blue","green", "yellow"];
+let randomChosenColour;
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+
+const nextSequence = () => {
+  let randomNumber = Math.round(Math.random()*3);
+  userClickedPattern = [];//empty user pattern...
+  randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+  playSound(randomChosenColour);
+  allowBtnClick();
+  console.log("game pattern  " + gamePattern);
+  $("h1").text(`LEVEL ${gamePattern.length}`);
+  return randomChosenColour;
+};
+
+const playSound = (name) => {
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+    animatePress(name);
+};
+
+//debugger;
+const animatePress = (currentColor) => {
+ $(`#${currentColor}`).addClass("pressed");
+ setTimeout(function(){ $(`#${currentColor}`).removeClass("pressed");},100);
+};
+
+const allowKeyDown = () => (
+  $(document).keydown(
+    function(){
+      nextSequence();
+      $(document).off("keydown");
+    }
+  )
+);
+
+allowKeyDown();
+
+const allowBtnClick =()=> (
+
+$(".btn").on("click", function(evt)
+   {
+      if (evt.target.id === gamePattern[userClickedPattern.length]) {
+          userClickedPattern.push(evt.target.id);
+          playSound(evt.target.id);
+          console.log("user pattern  " + userClickedPattern);
+          if (gamePattern.length === userClickedPattern.length) {
+            $(".btn").off("click");
+            setTimeout(nextSequence, 2000);
+        }
+      } else {
+        playSound("wrong");
+        $("h1").text("GAME OVER!!! \n PRESS A KEY TO RESTART");
+        $(".btn").off("click");
+        gamePattern = [];
+        allowKeyDown();
+
+      }
+    }
+  )
+);
+//  $("h1").text("GAME OVER!!!");
